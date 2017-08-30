b6c96e39098317e5806274afbf35701a9e902e43
Carter Dishman <cdishman001@student.butte.edu>
2017-08-30 01:33:03 +0000

    lab12 Commit

---
+++ Lab12/lab12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"        /\\       "<< endl;
+    cout<<"       ///\\      "<< endl;
+    cout<<"      ///()\\     "<< endl;
+    cout<<"     /()////\\    "<< endl;
+    cout<<"    /////()//\\   "<< endl;
+    cout<<"   /////////()\\  "<< endl;
+    cout<<"   v   | |       "<< endl;
+    cout<<" _[|]__| |__     "<< endl;  
+}
