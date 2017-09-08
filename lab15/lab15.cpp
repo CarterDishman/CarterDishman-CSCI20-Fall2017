9e30c8fa41286acd1996dc6f2f7acbe6e0d365c9
Carter Dishman <cdishman001@student.butte.edu>
2017-09-08 02:05:17 +0000

    lab15 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,104 @@
+//Created By: Carter Dishman   
+//Created On: 9/7/2017    
+//Story by MadLibs http://www.madlibs.com/printables/
+ 
+ #include <iostream> 
+ #include <string>
+ using namespace std;
+ 
+ int main()                         
+ {   
+    string madLibs1;
+    string madLibs2;
+    string madLibs3;
+    string madLibs4;
+    string madLibs5;
+    string madLibs6;
+    string madLibs7;
+    string madLibs8;
+    string madLibs9;
+    string madLibs10;
+    string madLibs11;
+    string madLibs12;
+    string madLibs13;
+    string madLibs14;
+    string madLibs15;
+    string madLibs16;
+    string madLibs17;
+    string madLibs18;
+    string madLibs19;
+    string madLibs20;
+    
+    cout << "Plese enter an adjective: ";
+    cin >> madLibs1; 
+    
+    cout << "Plese enter an adjective: ";
+    cin >> madLibs2;
+    
+    cout << "Plese enter an noun: ";
+    cin >> madLibs3;
+    
+    cout << "Plese enter an noun: ";
+    cin >> madLibs4;
+    
+    cout << "Plese enter a plural noun: ";
+    cin >> madLibs5;
+    
+    cout << "Plese enter a game: ";
+    cin >> madLibs6;
+    
+    cout << "Plese enter a plural noun: ";
+    cin >> madLibs7;
+    
+    cout << "Plese enter a verb ending in -ing: ";
+    cin >> madLibs8;
+    
+    cout << "Plese enter a verb ending in -ing: ";
+    cin >> madLibs9;
+    
+    cout << "Plese enter a plural noun: ";
+    cin >> madLibs10;
+    
+   cout << "Plese enter a verb ending in -ing: ";
+    cin >> madLibs11; 
+    
+    cout << "Plese enter an noun: ";
+    cin >> madLibs12;
+    
+    cout << "Plese enter an plant: ";
+    cin >> madLibs13;
+    
+    cout << "Plese enter an part of the body: ";
+    cin >> madLibs14;
+    
+    cout << "Plese enter a place: ";
+    cin >> madLibs15;
+    
+    cout << "Plese enter a verb ending in -ing: ";
+    cin >> madLibs16;
+    
+    cout << "Plese enter an adjective: ";
+    cin >> madLibs17;
+    
+    cout << "Plese enter a number: ";
+    cin >> madLibs18;
+    
+    cout << "Plese enter a plural noun: ";
+    cin >> madLibs19;
+    
+    
+    
+    
+    
+cout << "A vacation is when you take a trip to some " << madLibs1 << " place with your " << madLibs2 << "family." << endl; 
+cout << "Usually you go to some place that is near a/an" << madLibs3 << "or up on a/an" << madLibs4 <<"."<< endl;
+cout << "A good vacation place is one where you can ride" << madLibs5 << "or play" << madLibs6 << "or go hunting for "<< madLibs7 << "." << endl;
+cout << "I like to spend my time" << madLibs8 << "or" << madLibs9 << "." << endl;
+cout << "When parents go on a vacation, they spend their time eating three " << madLibs10 << " a day, and fathers play golf, and mothers sit around " << madLibs11 << "." << endl;
+cout << "Last summer, my little brother fell in a/an " << madLibs12 << " and got poison " << madLibs13 << "all over his "<< madLibs14 << "." << endl; 
+cout << "My family is going to go to (the) " << madLibs15 << ", and I will practice " << madLibs16 << "." << endl;
+cout << "Parents need vacations more than kids because parents are always very " << madLibs18 << " and because they have to work " << madLibs19 << " hours every day all year making enough " << madLibs20 << " to pay for the vacation." << endl;
+
+
+   return 0;
+ }                                  
