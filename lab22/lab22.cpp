44b64fa6edfc0f066e020fbd49244b3c934f77ae
Carter Dishman <cdishman001@student.butte.edu>
2017-09-22 01:29:28 +0000

    lab22 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,23 @@
+//Created By: Carter Dishman   
+//Created On: 9/21/2017      
+ 
+ #include <iostream> 
+ #include <cmath>
+ #include <ctime>
+ using namespace std;
+ 
+void randNum()
+{
+    srand(time(0));                                      //This part of the code seeds the random numner in time
+    int randNum = (rand()%101);                          //This line sets the cap number to 100
+    cout << "Your random number is: " << randNum;        //Thi line outputs the random number
+    
+}
+ 
+int main()
+ {
+    randNum();                                           //Calls the function from above
+ 
+ 
+     return 0;
+ }
